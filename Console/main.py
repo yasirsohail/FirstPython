effa489e6c6b25bd42a68a006a10de1fc718c4e7
yasirsohail <yasirchaudhary056@gmail.com>
2025-04-15 04:34:51 +0500

    update

--- Console/main.py
+++ Console/main.py
@@ -1,3 +1,11 @@
 print("hello world")
 print("headache")
 print("struggling to understand github")
+name = "Yasir"
+age = 24
+is_civil_engineer = True
+print("Name:",name)
+print("Age:",age)
+print("Is a civil Engineer:", is_civil_engineer)
+name2 = input("whats your name?")
+print("Hello,"+name2+"! how you doing?")
