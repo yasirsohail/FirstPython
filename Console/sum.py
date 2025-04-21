2618f455017c25f678567f393b9ef6e7ef0e2e6b
yasirsohail <yasirchaudhary056@gmail.com>
2025-04-22 03:20:02 +0500

    two new probelms

---
+++ Console/sum.py
@@ -0,0 +1,10 @@
+num1 = float(input("Enter the first number:"))
+num2 = float(input("Enter the second number:"))
+print("The Result", num1+num2)
+
+temperature = float(input("Whats is your temprature:"))
+
+if temperature > 37:
+    print("You have a fever")
+else:
+    print("You don't have a fever")
