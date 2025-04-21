93bffbf21f46756201be3d081327d3bad2808336
yasirsohail <yasirchaudhary056@gmail.com>
2025-04-22 02:29:32 +0500

    some practice

---
+++ Console/P2.py
@@ -0,0 +1,17 @@
+apple=20
+banana=25
+orange=30
+print("Total Price of Fruits:", sum([apple,banana,orange]))
+has_license= False
+
+if has_license:
+ print("You can Drive")
+else:
+ print("You can't Drive")
+
+carry_license= True
+
+if carry_license:
+ print("You can Drive")
+else:
+ print("You can't Drive")
---
+++ Console/Practice.py
@@ -0,0 +1,7 @@
+name= "Yasir"
+age= 25
+city= "Lahore"
+print("Name:",name)
+print("Age:",age)
+print("My City:",city)
+
